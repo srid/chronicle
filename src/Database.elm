633b2d680d39e6c3ad43f953bfa8c3c5eaa92ad9
Sridhar Ratnakumar <srid@srid.ca>
2015-05-14 22:58:27 -0700

    Keep json decoders close to model
    where it makes most sense

--- src/Database.elm
+++ src/Database.elm
@@ -1,13 +1,10 @@
 module Database where
 
-import Json.Decode  as J
-import Json.Decode exposing ((:=))
 import Task         exposing (Task, ThreadID, andThen)
 import Signal       exposing (Signal, Mailbox, mailbox, send)
 
 import Http             exposing (Error, get)
 
-import Util as U
 import Model
 import Controller
 
@@ -20,34 +17,10 @@ allUrl : String
 allUrl =
   "/" ++ tableName
 
-howDecoder : J.Decoder Model.How
-howDecoder =
-  let f s = case s of
-    "great"  -> Ok Model.Great
-    "good"  -> Ok Model.Good
-    "meh"  -> Ok Model.Meh
-    "bad"  -> Ok Model.Bad
-    "terrible"  -> Ok Model.Terrible
-    otherwise -> Err "Invalid `how` field" 
-  in
-    J.customDecoder J.string f
-
-feelingDecoder : J.Decoder Model.Feeling
-feelingDecoder = Model.Feeling
-  `J.map`    ("how"    := howDecoder)
-  `U.andMap` ("what" := J.string)
-  `U.andMap` ("trigger"  := J.string)
-  `U.andMap` ("notes"   := J.string)
-  `U.andMap` ("at"  := J.string)
-  `U.andMap` ("day"  := J.string)
-
-feelingsDecoder : J.Decoder Model.Model
-feelingsDecoder = J.list feelingDecoder
-
 
 -- XXX: must handle error case from `get` using Result x a
 getFeelings : Task Error ()
 getFeelings =
-  get feelingsDecoder allUrl
+  get Model.decodeModel allUrl
     `andThen` \feelings ->
       send (.address Controller.actions) <| Controller.Initialize feelings
--- src/Model.elm
+++ src/Model.elm
@@ -1,6 +1,10 @@
 module Model where
 
+import Json.Decode  as J
+import Json.Decode exposing ((:=))
+
 import Util exposing (groupBy)
+import Util as U
 
 type alias Model =
   List Feeling
@@ -10,11 +14,37 @@ type alias Feeling =
   , what    : String
   , trigger : String
   , notes   : String
-  , at      : String
+  , at      : String  -- XXX: must fix date time type
   , day     : String
   }
 
 type How = Great | Good | Meh | Bad | Terrible
+
+-- JSON decoders
+
+decodeHow : J.Decoder How
+decodeHow =
+  let f s = case s of
+    "great"  -> Ok Great
+    "good"  -> Ok Good
+    "meh"  -> Ok Meh
+    "bad"  -> Ok Bad
+    "terrible"  -> Ok Terrible
+    otherwise -> Err "Invalid `how` field"
+  in
+    J.customDecoder J.string f
+
+decodeFeeling : J.Decoder Feeling
+decodeFeeling = Feeling
+  `J.map`    ("how"    := decodeHow)
+  `U.andMap` ("what" := J.string)
+  `U.andMap` ("trigger"  := J.string)
+  `U.andMap` ("notes"   := J.string)
+  `U.andMap` ("at"  := J.string)
+  `U.andMap` ("day"  := J.string)
+
+decodeModel : J.Decoder Model
+decodeModel = J.list decodeFeeling
 
 initialModel : Model
 initialModel = []
